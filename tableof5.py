9eee108728d9c952da3319d4df9d41b9c6f90207
sangit-pokhrel <heypsycho339@gmail.com>
2025-07-10 12:29:27 +0545

    table of 5 code

---
+++ tableof5.py
@@ -0,0 +1,19 @@
+base = int(input("enter any number: "))
+
+for i in range(1, 11):
+    result = base * i
+    print(f"{base} x {i} = {result}")
+
+#write a program to calculate simple interest 
+# p = 1000
+# t= 2
+# r = 10
+# simpleInterest = (p*t*r)/100
+# print(simpleInterest)
+
+
+# write a program to print the sum of 2 number entered by the user 
+a = int(input("Enter first number:"))
+b = int(input("Enter Second number:"))
+c= a+b
+print(c)
